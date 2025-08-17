58c4b96042ae13628e0b1d8c359d5a171eadd439
Matthew Park <h.matthew.park@gmail.com>
2025-08-17 15:23:49 -0400

    Update darwin formula to v1.0.1
    - Fix broken download URLs pointing to v1.0.0
    - Update to working v1.0.1 release with binaries
    - Remove SHA256 requirement for testing

--- Formula/darwin.rb
+++ Formula/darwin.rb
@@ -3,23 +3,21 @@ class Darwin < Formula
   homepage "https://github.com/spatio-labs/darwin-cli"
   license "MIT"
 
+  version "1.0.1"
+
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/spatio-labs/darwin-cli/releases/download/v1.0.0/darwin-darwin-arm64"
-      sha256 "PLACEHOLDER_ARM64_SHA"
+      url "https://github.com/spatio-labs/darwin-cli/releases/download/v1.0.1/darwin-darwin-arm64"
     else
-      url "https://github.com/spatio-labs/darwin-cli/releases/download/v1.0.0/darwin-darwin-amd64"
-      sha256 "PLACEHOLDER_AMD64_SHA"
+      url "https://github.com/spatio-labs/darwin-cli/releases/download/v1.0.1/darwin-darwin-amd64"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/spatio-labs/darwin-cli/releases/download/v1.0.0/darwin-linux-arm64"
-      sha256 "PLACEHOLDER_LINUX_ARM64_SHA"
+      url "https://github.com/spatio-labs/darwin-cli/releases/download/v1.0.1/darwin-linux-arm64"
     else
-      url "https://github.com/spatio-labs/darwin-cli/releases/download/v1.0.0/darwin-linux-amd64"
-      sha256 "PLACEHOLDER_LINUX_AMD64_SHA"
+      url "https://github.com/spatio-labs/darwin-cli/releases/download/v1.0.1/darwin-linux-amd64"
     end
   end
 
